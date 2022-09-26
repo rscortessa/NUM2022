5c523064ade5b2dad065423099ad1b8dfd08cd54
Ronalito <rcortes@ictp.it>
2022-09-26 20:30:04 +0200

    Assignment 5

--- 5_class/Ass05.Cortes.f90
+++ 5_class/Ass05.Cortes.f90
@@ -1,34 +1,19 @@
 PROGRAM READING
+  USE STATISTICS
   REAL, DIMENSION(:), ALLOCATABLE :: T
   REAL, DIMENSION(:), ALLOCATABLE :: W
-  INTEGER :: i,j,m,u,Ta,Wa,ios
-  CHARACTER(100) :: check
-  u=10
-  i=0
-  j=0
-  OPEN(UNIT=u,IOSTAT=ios,FILE='numerical.dat',STATUS='old',ACTION='read')
-  DO
-     READ(u,'(a100)',IOSTAT=ios) check
-     IF (ios==0) THEN
-        i=i+1
-        IF (check(1:1) == '#') THEN
-           j=j+1
-        ENDIf 
-     ELSE
-        EXIT
-     ENDIF
-  END DO
+  REAL :: A,B,Ta,Wa
+  INTEGER :: i
 
-  ALLOCATE(T(i-j))
-  ALLOCATE(W(i-j))
+  i=initialize('numerical.dat',13)
 
-  DO m=1,i
-     READ(u,IOSTAT=ios,FILE=)
-  
-  PRINT*,i
-  
-#  USE STATISTICS(T,W,Ta,Wa)
-  
+  ALLOCATE(T(i))
+  ALLOCATE(W(i))
 
+  CALL store(T,W,i)
+
+  CALL average(T,W,i,Ta,Wa)
+
+  PRINT*,Ta,Wa
   
 END PROGRAM READING
---
+++ 5_class/statistical.f90
@@ -0,0 +1,69 @@
+MODULE STATISTICS
+  IMPLICIT NONE
+CONTAINS
+  INTEGER FUNCTION initialize(name,a) RESULT(i)
+    INTEGER :: ios,u
+    INTEGER, INTENT(IN) :: a
+    CHARACTER(a),INTENT(IN) :: name
+    CHARACTER(100) :: check
+    PRINT*,name
+    u=10
+    ios=0
+    i=0
+    OPEN(UNIT=u,IOSTAT=ios, FILE=name,STATUS='old',ACTION='read')
+    DO
+       READ(u,*,IOSTAT=ios)check
+           IF (ios==0) THEN
+              IF(check(1:1)/='#') THEN
+                 i=i+1
+              END IF
+           ELSE
+              EXIT
+           ENDIF
+        END DO
+    CLOSE(u)
+    PRINT*,i
+  END FUNCTION initialize
+
+  
+  SUBROUTINE store(T,W,i)
+
+    INTEGER, INTENT(INOUT) :: i
+    REAL ,DIMENSION(i), INTENT(INOUT)  :: T
+    REAL ,DIMENSION(i), INTENT(INOUT)  :: W
+    REAL :: A,B
+    INTEGER :: j,ios,u
+    
+    ios=0
+    j=1
+    u=10
+    
+    OPEN(UNIT=u,IOSTAT=ios, FILE='numerical.dat',STATUS='old',ACTION='read')
+    DO WHILE(j<=i)
+       READ(u,'(T6f7.5,T14f9.3)',IOSTAT=ios)A,B
+       IF (ios==0) THEN
+          PRINT*,A,B
+          T(j)=A
+          W(j)=B
+          j=j+1
+       ENDIF
+    END DO
+    CLOSE(u)
+  END SUBROUTINE store
+
+  SUBROUTINE average(T,W,i,Ta,Wa)
+    INTEGER, INTENT(INOUT) :: i
+    REAL ,DIMENSION(i), INTENT(INOUT)  :: T,W
+    REAL ,INTENT(INOUT) :: Ta,Wa
+    INTEGER ::j
+    Ta=0
+    Wa=0
+    DO j=1,i
+       Ta=Ta+T(i)
+       Wa=Wa+W(i)
+    END DO
+    Ta=Ta/i
+    Wa=Wa/i
+  END SUBROUTINE average
+  
+END MODULE STATISTICS
