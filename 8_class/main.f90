6a6b82800d4b126b5e71e4561b39e9bd2c24c437
Ronalito <rcortes@ictp.it>
2022-10-03 19:16:01 +0200

    Class

--- 8_class/main.f90
+++ 8_class/main.f90
@@ -1,9 +1,12 @@
 PROGRAM numerical
   USE secantmethod 
-  INTEGER, DIMENSION(3) ::ios,u,m,n
-  REAL :: a,b,c,d,eps
+  INTEGER, DIMENSION(3) ::ios,u
+  INTEGER :: i,j,m
+  REAL :: a,b,c,eps
   u=(/10,20,30/)
   ios=(/0,0,0/)
+  i=0
+  j=0
   PRINT*,'Give an estimate of the interval and the precision:'
   WRITE(6,'(a)',advance='no')"Xo="
   READ*,a
@@ -16,20 +19,28 @@ PROGRAM numerical
 
   OPEN(UNIT=u(3),IOSTAT=ios(3),FILE='Iterations.txt',STATUS='new',ACTION='write')
   IF(ios(1)==0) THEN
-     CALL secant(a,b,eps,0,u(1))
-     PRINT*,a,b
+     CALL secant(a,b,eps,i,u(1))
+     PRINT*,a,b,i
      ELSE
         PRINT*,'Delete your textfiles'
      ENDIF
   IF(ios(2)==0) THEN
-     CALL bisection(a,b,eps,0,u(2),1.0)
-     PRINT*,a,b
+     CALL bisection(a,b,eps,j,u(2),1.0)
+     PRINT*,a,b,j
      ELSE
         PRINT*,'Delete your textfiles'
      ENDIF
   CLOSE(u(1))
   CLOSE(u(2))
 
+  DO m=-20,13
+     c=m*1.0/10
+     CALL secant(c,2.0,eps,i,u(1))
+     CALL bisection(c,2.0,eps,j,u(2),1.0)
+     WRITE(u(3),*)m*1.0/10,i,j
+     i=0
+     j=0
+  END DO
   
   CLOSE(u(3))
      
--- 8_class/newton-raphson.f90
+++ 8_class/newton-raphson.f90
@@ -6,12 +6,13 @@ CONTAINS
     REAL :: x
     fx=2*EXP(x)-2*x**2-4 ! DEFINE THE FUNCTION                                                                                       
   END FUNCTION f
-
+ 
   RECURSIVE SUBROUTINE secant(xn,xn1,eps,n,u)
     IMPLICIT NONE
-    REAL, INTENT(INOUT) :: xn,xn1,eps
+    REAL, INTENT(IN) :: xn,xn1,eps
     REAL :: xn2
-    INTEGER, INTENT(IN) :: n,u
+    INTEGER, INTENT(IN) :: u
+    INTEGER, INTENT(INOUT) :: n
     WRITE(u,*)n,xn1
     IF(ABS(f(xn1)-f(xn))<TINY(xn)) THEN
        PRINT*,'The root cannot be determined'
@@ -21,16 +22,17 @@ CONTAINS
     IF(ABS(xn2-xn1)<eps) THEN
        RETURN
     ELSE
-       CALL secant(xn1,xn2,eps,n+1,u)
+       n=n+1
+       CALL secant(xn1,xn2,eps,n,u)
     ENDIF
   END SUBROUTINE secant
 
     
   RECURSIVE SUBROUTINE bisection(a,b,eps,i,u,m) 
     IMPLICIT NONE
-    REAL, INTENT(IN) :: a,b,eps,m
-    INTEGER, INTENT(IN) ::i,u
-    INTEGER :: j
+    REAL, INTENT(IN) ::a,b,eps,m
+    INTEGER, INTENT(IN) :: u
+    INTEGER, INTENT(INOUT):: i
     REAL :: c
     IF (a>b .or. eps<0) THEN !INITIAL COMDITIONS
        PRINT*,'Define a good interval or precision'
@@ -44,12 +46,14 @@ CONTAINS
     ENDIF
     
     IF(SIGN(1.0,f(b))*SIGN(1.0,f(c))<0) THEN
-       CALL bisection(c,b,eps,i+1,u,m)
+       i=i+1
+       CALL bisection(c,b,eps,i,u,m)
     ELSE
-       CALL bisection(a,c,eps,i+1,u,m)
+       i=i+1
+       CALL bisection(a,c,eps,i,u,m)
     ENDIF
   END SUBROUTINE bisection
-
+  
 END MODULE secantmethod
     
 
